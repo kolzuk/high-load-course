fa39a97e158cd8aa07053202c4061371cc61b71c
Aital Bagardynov <aytalbagardynov@mail.ru>
2025-11-21 18:07:54 +0300

    add dbScope and remove ratelimiter

--- src/main/kotlin/ru/quipy/common/utils/webclient/WebClientConfig.kt
+++ src/main/kotlin/ru/quipy/common/utils/webclient/WebClientConfig.kt
@@ -3,12 +3,28 @@ package ru.quipy.common.utils.webclient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import reactor.netty.http.HttpProtocol
 import reactor.netty.http.client.HttpClient
 import reactor.netty.resources.ConnectionProvider
+import ru.quipy.apigateway.errors.TooManyRequestsException
+import ru.quipy.common.utils.SlidingWindowRateLimiter
 
 @Configuration
 class WebClientConfig {
+    fun rateLimitingFilter(rateLimiter: SlidingWindowRateLimiter): ExchangeFilterFunction =
+        ExchangeFilterFunction.ofRequestProcessor { req: ClientRequest ->
+            if (rateLimiter.tick()) {
+                Mono.just(req)
+            } else {
+                Mono.error(TooManyRequestsException())
+            }
+        }
+
+
     @Bean
     fun webClient(): WebClient {
         val connectionProvider = ConnectionProvider
@@ -16,7 +32,9 @@ class WebClientConfig {
             .maxConnections(10_000)
             .build()
 
-        val httpClient = HttpClient.create(connectionProvider)
+        val httpClient = HttpClient
+            .create(connectionProvider)
+            .protocol(HttpProtocol.H2C)
 
         return WebClient
             .builder()
--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -3,10 +3,16 @@ package ru.quipy.payments.logic
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.micrometer.core.instrument.MeterRegistry
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.reactor.awaitSingle
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
@@ -41,16 +47,6 @@ class PaymentExternalSystemAdapterImpl(
     private val rateLimitPerSec = properties.rateLimitPerSec
     private val parallelRequests = properties.parallelRequests
 
-    private val rateLimiter = SlidingWindowRateLimiter((rateLimitPerSec).toLong())
-
-    private inline fun <reified T> rateLimited(mono: Mono<T>): Mono<T> =
-        Mono.fromCallable { rateLimiter.tick() }
-            .subscribeOn(Schedulers.boundedElastic())
-            .flatMap { permitted ->
-                if (permitted) mono
-                else Mono.error(TooManyRequestsException())
-            }
-
     override suspend fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
         performPaymentAsync(paymentId, amount, paymentStartedAt, deadline, attempts = 0)
     }
@@ -61,58 +57,69 @@ class PaymentExternalSystemAdapterImpl(
 
     override fun name() = properties.accountName
 
+    @OptIn(DelicateCoroutinesApi::class)
+    private val dbScope = CoroutineScope(
+        newFixedThreadPoolContext(100, "db_pool")
+    )
+
     private suspend fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long, attempts: Int) {
         logger.warn("[$accountName] Submitting payment request for payment $paymentId")
 
         val transactionId = UUID.randomUUID()
 
-        // Вне зависимости от исхода оплаты важно отметить что она была отправлена.
-        // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования.
-        paymentESService.update(paymentId) {
-            it.logSubmission(success = true, transactionId, now(), Duration.ofMillis(now() - paymentStartedAt))
+        dbScope.launch {
+            // Вне зависимости от исхода оплаты важно отметить что она была отправлена.
+            // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования.
+            paymentESService.update(paymentId) {
+                it.logSubmission(success = true, transactionId, now(), Duration.ofMillis(now() - paymentStartedAt))
+            }
         }
 
         logger.info("[$accountName] Submit: $paymentId , txId: $transactionId")
 
         try {
-            val request = webClient
+            val response = webClient
                 .post()
                 .uri("http://$paymentProviderHostPort/external/process?serviceName=$serviceName&token=$token&accountName=$accountName&transactionId=$transactionId&paymentId=$paymentId&amount=$amount")
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .toEntity(ExternalSysResponse::class.java)
-
-            val response = rateLimited(request)
                 .awaitSingle()
 
             logger.info("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, message: ${response.body?.result}, result code: ${response.statusCode}")
 
-            // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
-            // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
-            paymentESService.update(paymentId) {
-                it.logProcessing(
-                    response.body!!.result, now(), transactionId, reason = response.body!!.message
-                )
+            dbScope.launch {
+                // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
+                // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
+                paymentESService.update(paymentId) {
+                    it.logProcessing(
+                        response.body!!.result, now(), transactionId, reason = response.body!!.message
+                    )
+                }
             }
         } catch (e: Exception) {
             when (e) {
                 is SocketTimeoutException -> {
                     logger.error("[$accountName] Payment timeout for txId: $transactionId, payment: $paymentId", e)
-                    paymentESService.update(paymentId) {
-                        it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
+                    dbScope.launch {
+                        paymentESService.update(paymentId) {
+                            it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
+                        }
                     }
                 }
-                is TooManyRequestsException -> {
-                    logger.error("[$accountName] Too many requests for txId: $transactionId, payment: $paymentId")
-                    if (attempts < MAX_ATTEMPTS && deadline - now() > requestAverageProcessingTime.toMillis()) {
-                        performPaymentAsync(paymentId, amount, paymentStartedAt, deadline, attempts + 1)
-                    }
-                }
+                // is TooManyRequestsException -> {
+                //     logger.error("[$accountName] Too many requests for txId: $transactionId, payment: $paymentId")
+                //     if (attempts < MAX_ATTEMPTS && deadline - now() > requestAverageProcessingTime.toMillis()) {
+                //         performPaymentAsync(paymentId, amount, paymentStartedAt, deadline, attempts + 1)
+                //     }
+                // }
                 else -> {
                     logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
 
-                    paymentESService.update(paymentId) {
-                        it.logProcessing(false, now(), transactionId, reason = e.message)
+                    dbScope.launch {
+                        paymentESService.update(paymentId) {
+                            it.logProcessing(false, now(), transactionId, reason = e.message)
+                        }
                     }
                 }
             }
